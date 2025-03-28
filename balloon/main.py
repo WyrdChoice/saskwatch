89ea4d210d9d25f3a22c32c1f4303cc1b958d8be
WyrdChoice <liamaccadieux@gmail.com>
2025-03-28 12:50:41 -0400

    Update main.py

--- balloon/main.py
+++ balloon/main.py
@@ -1,6 +1,6 @@
 # from datetime import datetime
 import json
-from machine import UART, Pin
+from machine import UART, Pin, ADC
 
 # from bme680 import *
 from time import sleep
@@ -22,6 +22,40 @@ def send_at_command(command, response_timeout=0.1):
     else:
         print("No response from BLE module.")
 
+adc = ADC(26)  # ADC0 corresponds to GP26 on Pico
+
+def get_voltage(pin):
+    return (pin.read_u16() * 3.3) / 65535  # Convert digital value to analog voltage
+
+def wind_speed(voltage):
+    return voltage  # Linear assumption, may need adjustment
+
+solenoid_relay = Pin(16, Pin.OUT)  # GP16 as output
+def trigger_pin(delay):
+    solenoid_relay.value(1)
+    sleep(delay)
+    solenoid_relay.value(0)
+
+# Define rotary encoder pins
+DT_Pin = Pin(0, Pin.IN, Pin.PULL_UP)  # GP0 as input with pull-up
+CLK_Pin = Pin(1, Pin.IN, Pin.PULL_UP)  # GP1 as input with pull-up
+
+value = 0
+previous_value = 1
+
+def rotary_changed():
+    global previous_value, value
+
+    if previous_value != CLK_Pin.value():
+        if CLK_Pin.value() == 0:
+            if DT_Pin.value() == 0:
+                value = (value - 1) % 20
+                print("anti-clockwise", value)
+            else:
+                value = (value + 1) % 20
+                print("clockwise", value)
+        previous_value = CLK_Pin.value()
+
 
 send_at_command("+++")
 send_at_command("ATI")
@@ -40,8 +74,12 @@ while True:
     # msg = f"[START]{data}[END]"
     # BLEuart.write(msg)
     # print(msg)
+    windSpeed = wind_speed()
+    rotary_changed(get_voltage(adc))
+
+
     data = json.dumps(
-        {"moduleID": 2, "moduleName": "Balloon", "count": count, "message": "hello"}
+        {"moduleID": 2, "moduleName": "Balloon", "count": count, "message": "hello", "Direction": value*18, "windspeed": windSpeed}
     )
 
     msg = f"[START]{data}[END]"
